b28cbf4f4592137960bb57fee81b3b763811aa0b
georginamungai <georgienduts@gmail.com>
2022-08-17 10:44:35 +0300

    Added Classes

---
+++ python/api/models/Doctor.py
@@ -0,0 +1,9 @@
+class Doctor:
+    def __init__(self,FullName, Telephone, Email, Password, Location, Religion):
+        self.FullName = FullName
+        self.Telephone = Telephone
+        self.Email = Email
+        self.Password = Password
+        self.Location = Location
+        self.Religion = Religion
+        pass
