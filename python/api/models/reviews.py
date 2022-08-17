a0472f55afe99ef3a81b9b89ed1aaa6baa28baef
georginamungai <georgienduts@gmail.com>
2022-08-17 13:30:13 +0300

    Created Counselling and Review Files

---
+++ python/api/models/reviews.py
@@ -0,0 +1,6 @@
+class Reviews:
+    def __init__(self, Patient_Id, Doctor_Id):
+        self.Patient_Id = Patient_Id
+        self.Doctor_Id = Doctor_Id
+        self.Review = str(' ')
+    def rate (self):
