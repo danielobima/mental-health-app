12c21263be2a29e643b230491e820f59503c11d8
danielobima <demannman67@gmail.com>
2022-11-08 11:20:14 +0300

    another test

--- python/api/__init__.py
+++ python/api/__init__.py
@@ -15,6 +15,7 @@ def create_app(test_config=None):
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
+    print("Hello world")
     print(os.environ.get("FB_PROJECT_ID"))
     cred = credentials.Certificate(
         {
