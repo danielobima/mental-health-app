dcc19c00ee4b0fc5130dbefcc9f7127e62751e1b
danielobima <demannman67@gmail.com>
2022-11-08 11:17:12 +0300

    testing

--- python/api/__init__.py
+++ python/api/__init__.py
@@ -12,28 +12,31 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
-    cred = credentials.Certificate({
-        "type": "service_account",
-  "project_id": os.environ.get('FB_PROJECT_ID'),
-  "private_key_id": os.environ.get('FB_PRIVATE_KEY_ID'),
-  "private_key": os.environ.get('FB_PRIVATE_KEY'),
-  "client_email": os.environ.get('FB_CLIENT_EMAIL'),
-  "client_id": os.environ.get('FB_CLIENT_ID'),
-  "auth_uri": os.environ.get('FB_AUTH_URI'),
-  "token_uri": os.environ.get('FB_TOKEN_URI'),
-  "auth_provider_x509_cert_url":os.environ.get('FB_AUTH_CERT_URL'),
-  "client_x509_cert_url":os.environ.get('FB_CLIENT_CERT_URL')
-    })
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': os.environ.get('FB_DATABASE_URL')
-    })
+    print(os.environ.get("FB_PROJECT_ID"))
+    cred = credentials.Certificate(
+        {
+            "type": "service_account",
+            "project_id": os.environ.get("FB_PROJECT_ID"),
+            "private_key_id": os.environ.get("FB_PRIVATE_KEY_ID"),
+            "private_key": os.environ.get("FB_PRIVATE_KEY"),
+            "client_email": os.environ.get("FB_CLIENT_EMAIL"),
+            "client_id": os.environ.get("FB_CLIENT_ID"),
+            "auth_uri": os.environ.get("FB_AUTH_URI"),
+            "token_uri": os.environ.get("FB_TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.environ.get("FB_AUTH_CERT_URL"),
+            "client_x509_cert_url": os.environ.get("FB_CLIENT_CERT_URL"),
+        }
+    )
+    firebase_admin.initialize_app(
+        cred, {"databaseURL": os.environ.get("FB_DATABASE_URL")}
+    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -50,6 +53,5 @@ def create_app(test_config=None):
     app.register_blueprint(session.bp)
     app.register_blueprint(patient.bp)
     app.register_blueprint(doctor.bp)
-
 
     return app
