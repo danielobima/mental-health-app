92c0eeac8eaeab469e9f6e09e985fbf5b45ed829
danielobima <demannman67@gmail.com>
2022-11-10 14:58:30 +0300

    changed base url

--- src/index.js
+++ src/index.js
@@ -31,4 +31,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-export const baseURL = "http://localhost:5000";
+export const baseURL = "https://therapy4you-server.herokuapp.com";
